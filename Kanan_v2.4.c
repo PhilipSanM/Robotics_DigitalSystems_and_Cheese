1f43b6431c7fcadf0d8072d6af72b895da617ea9
Felipe Sanchez Martinez <felipesanchezmart@gmail.com>
2022-05-20 22:57:58 -0500

    Update Kanan_v2.4.c

--- Kanan_v2.4.c
+++ Kanan_v2.4.c
@@ -12,10 +12,10 @@
        
 Mexico is one of the most insecure countries, in addition to having a large number of missing persons... 
 Have you ever felt in danger? 
-(This is a project that I carried out during my confinement due to the covid-19 pandemic)
+(This is a project carried out during my confinement due to the covid-19 pandemic)
 
 :3
-
+* @copyright Copyright (c) 2022
 */
 
 
